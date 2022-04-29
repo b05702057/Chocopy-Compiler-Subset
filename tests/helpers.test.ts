1b0c2af9d5f1218e54cef71d711784e67ad570e8
李季澄 <b05702057@ntu.edu.com>
2022-04-29 08:28:39 -0700

    check helper is called

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -5,19 +5,20 @@ import { importObject } from "./import-object.test";
 
 // Modify typeCheck to return a `Type` as we have specified below
 export function typeCheck(source: string) : Type {
-  const program = typeCheckProgram(parse(source));
-  if (program.stmts.length === 0) {
-    return "none";
-  }
-  const lastStatement = program.stmts[program.stmts.length - 1];
-  if (lastStatement.tag == "expr") {
-    if (lastStatement.expr.a === "None") {
-      return "none";
-    }
-    // convert the type for testing
-    return lastStatement.expr.a;
-  }
-  return "none";
+  throw Error(`check if typeCheck is called correctly ${source}`);
+  // const program = typeCheckProgram(parse(source));
+  // if (program.stmts.length === 0) {
+  //   return "none";
+  // }
+  // const lastStatement = program.stmts[program.stmts.length - 1];
+  // if (lastStatement.tag == "expr") {
+  //   if (lastStatement.expr.a === "None") {
+  //     return "none";
+  //   }
+  //   // convert the type for testing
+  //   return lastStatement.expr.a;
+  // }
+  // return "none";
 }
 
 // Modify run to use `importObject` (imported above) to use for printing
@@ -25,6 +26,7 @@ export function typeCheck(source: string) : Type {
 // within another function in your compiler, for example if you need other
 // JavaScript-side helpers
 export async function run(source: string) {
+  throw Error(`check if run is called correctly ${source}`);
   // Create the memory and add it to impots
   var memory = new WebAssembly.Memory({initial:10, maximum:100});
   (importObject.imports as any).mem = memory
